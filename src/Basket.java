af0480abe9c0526fab3d0d63bc49976cd0c704ff
kmzplanner <kmzplanner@yandex.ru>
2023-05-07 11:34:18 +0300

    Initial commit

---
+++ src/Basket.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Basket {
+    private String[] goods;
+    private int[] prices;
+    private int[] quantities;
+
+    public Basket() {
+    }
+
+    public Basket(String[] goods, int[] prices) {
+        this.goods = goods;
+        this.prices = prices;
+        this.quantities = new int[goods.length];
+    }
+
+    public void addToCart(int productNum, int amount) {
+        quantities[productNum] += amount;
+    }
+
+    public void printCart() {
+        int totalPrice = 0;
+        System.out.println("Список пуст");
+        for (int i = 0; i < goods.length; i++) {
+            if(quantities[i] > 0) {
+                int currentPrice = prices[i] * quantities[i];
+                totalPrice += currentPrice;
+                System.out.printf("%15s%4d р/шт%4d шт%6d p%n", goods[i], prices[i], quantities[i], currentPrice);
+            }
+        }
+        System.out.printf("Итого: %dp", totalPrice);
+    }
+
+    public void saveTxt(File textFile) throws FileNotFoundException {
+        try(PrintWriter out = new PrintWriter(textFile)) {
+            for (String good : goods) {
+                out.print(good + " ");
+            }
+            out.println();
+
+            for (int price : prices) {
+                out.print(price + " ");
+            }
+            out.println();
+
+            for (int quantity : quantities) {
+                out.print(quantity + " ");
+            }
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) {
+        Basket basket = new Basket();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(textFile))) {
+            String goodsStr = bufferedReader.readLine();
+            String prisesStr = bufferedReader.readLine();
+            String quantitiesStr = bufferedReader.readLine();
+
+            basket.goods = goodsStr.split(" ");
+            basket.prices = Arrays.stream(prisesStr.split(" "))
+                    .map(Integer::parseInt)
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+
+            basket.quantities = Arrays.stream(quantitiesStr.split(" "))
+                    .map(Integer::parseInt)
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return basket;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+    static int[] prices = {100, 200, 300};
+    static String[] products = {"Хлеб", "Яблоко", "Молоко"};
+    static File saveFile = new File("basket.txt");
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Basket basket = null;
+
+        if (saveFile.exists()) {
+            basket = Basket.loadFromTxtFile(saveFile);
+        } else {
+            basket = new Basket(products, prices);
+        }
+
+        while (true) {
+            showPrice();
+            System.out.println("Выбирите товар и количество через пробел или на бирите end");
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
+                break;
+            }
+
+            String[] parts = input.split(" ");
+            int productNumber = Integer.parseInt(parts[0]) - 1;
+            int productCount = Integer.parseInt(parts[1]);
+            basket.addToCart(productNumber, productCount);
+            basket.saveTxt(saveFile);
+        }
+
+        basket.printCart();
+
+    }
+
+    public static void showPrice() {
+        System.out.println("Список возможных товаров для покупки");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println(products[i] + " " + prices[i] + " руб./шт");
+        }
+    }
+}
+
